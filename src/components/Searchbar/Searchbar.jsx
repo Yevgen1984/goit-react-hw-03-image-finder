1fbcade67766d08965f5d4fe3a626e4898c11b00
Yevgen <107062952+Yevgen1984@users.noreply.github.com>
2022-10-16 15:59:55 +0300

    add correction HW3 (part 2 comment Note)

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Component } from 'react';
 import { BsSearch } from 'react-icons/bs';
-import { Note } from '../ImageGallery/Notification';
+// import { Note } from '../ImageGallery/Notification';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import s from './Searchbar.module.css';
